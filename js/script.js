7d2f1347e8bf03b18f4901203966780d818a3777
Adrian Pelaez <adrianpt1991@gmail.com>
2023-07-19 13:10:44 -0400

    Add home section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
+const main = document.querySelector('main');
+const logoWhite = document.querySelector('.white');
+const logoBlack = document.querySelector('.black');
 menuIcon.addEventListener('click', function() {
 	menuIcon.classList.toggle('rotate');
 	menu.classList.toggle('show');
+
+	const isVisible = window.getComputedStyle(logoWhite).display !== 'none';
+
+	if (isVisible) {
+		logoWhite.style.display = 'none'; 
+		logoBlack.style.display = 'block';
+	} else {
+		logoWhite.style.display = 'block'; 
+		logoBlack.style.display = 'none'; 
+	}
 });
